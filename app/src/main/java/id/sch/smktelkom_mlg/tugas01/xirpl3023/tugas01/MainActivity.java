c75a8816f35615d28002b24452b6e73b3b4d9913
Nabilla Elbashra <nabillabasharahil@gmail.com>
2016-09-12 18:02:12 +0700

    deklarasi variabel

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3023/tugas01/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3023/tugas01/MainActivity.java
@@ -2,12 +2,24 @@ package id.sch.smktelkom_mlg.tugas01.xirpl3023.tugas01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity
 {
+    EditText etNama, etSewa, etFasT;
+    RadioButton rbS, rbD, rbSS;
+    TextView tvHasil;
+    CheckBox cbI, cbTV, cbAC;
+    Spinner spKode;
+    Button bOK;
 
 
     @Override
